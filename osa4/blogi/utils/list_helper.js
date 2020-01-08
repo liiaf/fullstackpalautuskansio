aa74d27a3cb0be95b4f4a391c3a7c14f9ea666b2
Liia Flinkman <liia.flinkman@gmail.com>
2020-01-08 20:49:13 +0100

    tehtävä 4.4

--- osa4/blogi/utils/list_helper.js
+++ osa4/blogi/utils/list_helper.js
@@ -1,7 +1,23 @@
-const dummy = (blogs) => {
+const dummy = () => {
     return 1
 }
+
+const totalLikes = (blogs) => {
+    if (blogs.length === 0){
+      return 0
+    }
+    const summa = (sum, blog) => {
+        return sum + blog.likes
+    }
+    const total = blogs.reduce(summa, 0);
+    return total
+  }
+
+
   
   module.exports = {
-    dummy
+    dummy,
+    totalLikes
   }
+
+
